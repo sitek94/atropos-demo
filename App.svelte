877a1506a16bc1d913162dd47ec69c1bdb19c13e
Maciek Sitkowski <58401630+sitek94@users.noreply.github.com>
2022-04-16 08:30:39 +0200

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Atropos from 'atropos/svelte'
+</script>
+
+<main class="container">
+  <Atropos class="atropos-banner" highlight={false} on:enter={() => console.log('enter')}>
+    <img class="atropos-banner-spacer" src="/atropos-bg.svg" alt="" />
+    <img data-atropos-offset="-4.5" src="/atropos-bg.svg" alt="" />
+    <img data-atropos-offset="-2.5" src="/atropos-mountains.svg" alt="" />
+    <img data-atropos-offset="0" src="/atropos-forest-back.svg" alt="" />
+    <img data-atropos-offset="2" src="/atropos-forest-mid.svg" alt="" />
+    <img data-atropos-offset="4" src="/atropos-forest-front.svg" alt="" />
+    <img data-atropos-offset="5" src="/atropos-logo-en.svg" alt="" />
+  </Atropos>
+</main>
---
+++ index.js
@@ -0,0 +1,10 @@
+import 'atropos/css'
+
+import App from './App.svelte'
+import './index.css'
+
+const app = new App({
+  target: document.querySelector('#app'),
+})
+
+export default app
